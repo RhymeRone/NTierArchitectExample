b63729e241cb06665337aedf6722bda64d469379
ismail hallac <mavi-okyanus63@hotmail.com>
2024-02-17 20:25:23 +0300

    -mimari yapi klasörleri eklendi -entitiesler eklendi -data access / abstract / instructor tanımlandı

---
+++ DataAccess/Abstract/IInstructorDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NTierArchitectExample.Entities;
+
+namespace NTierArchitectExample.DataAccess.Abstract
+{
+    public interface IInstructorDal
+    {
+        public void Add(Instructor instructor);
+        public void Update(Instructor instructor);
+        public void Delete(int Id);
+        public Instructor GetInstructor(int Id);
+        public List<Instructor> GetAll();
+    }
+}
